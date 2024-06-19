5161a677305fe1be4f413288eae68f1340e83ea3
Zal <80491337+Zal2024@users.noreply.github.com>
2024-06-19 17:11:57 +0700

    Add files via upload

---
+++ ask.py
@@ -0,0 +1,22 @@
+#ask
+
+ask= input("Are you hungry? ")
+
+if "y" or "Y" in ask: #Yes
+    print("Go eat food")
+else: #No
+ print("Ok")
+ ask2 = input("what food did you ate? ")
+ print("Cool")
+ 
+print("""
+      
+      
+      
+      
+      
+      
+Closing in 5 seconds""")
+
+import time
+time.sleep(5)
